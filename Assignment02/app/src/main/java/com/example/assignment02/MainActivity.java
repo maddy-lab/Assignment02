910adfdc4edb4f327826e018a2cb154971ea4666
Madhan <69673888+maddy-lab@users.noreply.github.com>
2024-09-03 14:01:12 -0400

    final changes

--- Assignment02/app/src/main/java/com/example/assignment02/MainActivity.java
+++ Assignment02/app/src/main/java/com/example/assignment02/MainActivity.java
@@ -13,35 +13,41 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
-    private View colorView;
+    private View colorView ;
     private TextView rgbValue;
-    private TextView HEXValue;
+    private TextView HEXValue ;
     private SeekBar redSeekbar;
-    private SeekBar greenSeekbar;
-    private SeekBar blueSeekbar;
-
+    private SeekBar greenSeekbar ;
+    private SeekBar blueSeekbar ;
+    private TextView redProgressValue;
+    private TextView greenProgressValue;
+    private TextView blueProgressValue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        colorView = findViewById(R.id.colorView);
-        rgbValue = findViewById(R.id.rgb_value);
-        HEXValue = findViewById(R.id.HEX_value);
-        redSeekbar = findViewById(R.id.redSeekbar);
-        greenSeekbar = findViewById(R.id.greenSeekbar);
-        blueSeekbar = findViewById(R.id.blueSeekbar);
+        colorView=findViewById(R.id.colorView);
+        rgbValue=findViewById(R.id.rgb_value);
+        HEXValue=findViewById(R.id.HEX_value);
+        redSeekbar=findViewById(R.id.redSeekbar);
+        blueSeekbar=findViewById(R.id.blueSeekbar);
+        greenSeekbar=findViewById(R.id.greenSeekbar);
+        redProgressValue=findViewById(R.id.redValue);
+        blueProgressValue=findViewById(R.id.blueValue);
+        greenProgressValue=findViewById(R.id.greenValue);
 
         redSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
                 updateColor();
+                redProgressValue.setText(String.valueOf(progress));
             }
 
             @Override
@@ -56,8 +62,9 @@ public class MainActivity extends AppCompatActivity {
         });
         blueSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
                 updateColor();
+                blueProgressValue.setText(String.valueOf(progress));
             }
 
             @Override
@@ -72,8 +79,9 @@ public class MainActivity extends AppCompatActivity {
         });
         greenSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
                 updateColor();
+                greenProgressValue.setText(String.valueOf(progress));
             }
 
             @Override
@@ -86,54 +94,30 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
-        findViewById(R.id.reset_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-               reset();
-            }
-        });
-        findViewById(R.id.black_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                changeColor(0, 0, 0);
-            }
-        });
-        findViewById(R.id.white_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                MainActivity.this.changeColor(255, 255, 255);
-            }
-        });
-        findViewById(R.id.blue_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                MainActivity.this.changeColor(0, 0, 255);
-            }
-        });
-
-        updateColor();
+        findViewById(R.id.reset_button).setOnClickListener(view -> reset());
+        findViewById(R.id.black_button).setOnClickListener(view -> changeColor(0,0,0));
+        findViewById(R.id.white_button).setOnClickListener(view -> changeColor(255,255,255));
+        findViewById(R.id.blue_button).setOnClickListener(view -> changeColor(0,0,255));
     }
 
-    private void updateColor() {
-        int red = redSeekbar.getProgress();
-        int blue = blueSeekbar.getProgress();
-        int green = greenSeekbar.getProgress();
-        int color = Color.rgb(red, green, blue);
+    private void updateColor()
+    {
+        int red=redSeekbar.getProgress();
+        int blue=blueSeekbar.getProgress();
+        int green=greenSeekbar.getProgress();
+        int color = Color.rgb(red,green,blue);
         colorView.setBackgroundColor(color);
-        rgbValue.setText(" (" + red + ", " + green + ", " + blue + ")");
-        HEXValue.setText(String.format(" #%02X%02X%02X", red, green, blue));
+        rgbValue.setText("("+red+","+green+","+blue+")");
+        HEXValue.setText(String.format("#%02X%02X%02X",red,green,blue));
     }
-
-    private void reset() {
-
+    private void reset(){
         redSeekbar.setProgress(64);
         greenSeekbar.setProgress(128);
         blueSeekbar.setProgress(0);
         updateColor();
     }
-
-    private void changeColor(int red, int green, int blue) {
+    private void changeColor(int red,int green, int blue)
+    {
         redSeekbar.setProgress(red);
         blueSeekbar.setProgress(blue);
         greenSeekbar.setProgress(green);
